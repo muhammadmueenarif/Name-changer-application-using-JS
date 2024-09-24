939d3178a7fb2e300c69ac7a5c6d481fadf5df84
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-09-24 20:02:44 +0500

    name changer application using js

---
+++ script.js
@@ -0,0 +1,19 @@
+function changeText() {
+    var name = prompt("Enter your name");
+    document.getElementById("world").innerHTML = name + '!';
+}
+
+
+function changeText() {
+    var name = prompt("Enter your name");
+    document.getElementById("world").innerHTML = name + '!';
+}
+
+//we can also do this by queryselector. 
+// const btnElement = document.querySelector('button');
+// const world = document.getElementById('world');
+
+// btnElement.onclick = function () {
+//     const username= prompt("");
+//     world.textContent = username;
+// }
